1f9eb9a392c2b0e64170493f1eb4d0aef2255a67
Zadyy <nyambayar2014@gmail.com>
2023-11-22 22:33:58 +0800

    added second query

--- index.php
+++ index.php
@@ -13,8 +13,11 @@ if ($conn->connect_error) {
 }
 
 // Fetch items from the database
-$sql = "SELECT * FROM iteminfo";
-$result = $conn->query($sql);
+$sql1 = "SELECT * FROM iteminfo";
+$result1 = $conn->query($sql1);
+
+$sql2 = "SELECT * FROM origin";
+$result2 = $conn->query($sql2);
 
 ?>
 
@@ -31,14 +34,22 @@ $result = $conn->query($sql);
 
     <?php
     // Check if there are items in the result set
-    if ($result->num_rows > 0) {
+    if ($result1->num_rows > 0) {
         // Output data of each row
-        while($row = $result->fetch_assoc()) {
+        while($row = $result1->fetch_assoc()) {
             echo "<p><strong>ID:</strong> " . $row["itemid"]. " - <strong>Name In English:</strong> " . $row["nameen"]. " - <strong>Name In Mongolian:</strong> " . $row["namemn"].  " - <strong>Wikipedia Link:</strong> " . $row["wikilink"]. " - <strong>Type:</strong> " . $row["type"]."</p>";
+        }
+
+        if ($result2->num_rows > 0) {
+            while($row2 = $result2->fetch_assoc()) {
+                echo "<p><strong>ID:</strong> " . $row["itemid"]. " - <strong>Country:</strong> " . $row["countryoforigin"]. " - <strong>Condition:</strong> " . $row["condition1"]. " - <strong>Total Weight:</strong> " . $row["weight"]. " - <strong>Unit:</strong> " . $row["unit"]. " - <strong>Shipped via:</strong> " . $row["shippedby"]. " - <strong>Number of container:</strong> " . $row["quantity"]. "</p>";
+            }
         }
     } else {
         echo "0 results";
     }
+
+
 
     // Close the database connection
     $conn->close();
